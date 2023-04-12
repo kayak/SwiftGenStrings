744f6d48558dfaac07d90598d75c7bec535a7d2d
Henrik Panhans <henrik@panhans.dev>
2023-04-12 10:30:31 +0200

    Update ArgumentParser to newest version
    Signed-off-by: Henrik Panhans <henrik@panhans.dev>

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.3.0")
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
